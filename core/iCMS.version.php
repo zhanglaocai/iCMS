bd5cece94d692571b383df18a4c6ca43f3cb4b37
cool.tea <idreamsoft@qq.com>
2015-05-11 15:01:21 +0800

    VERSION V6.0.4 RELEASE 20150511

--- core/iCMS.version.php
+++ core/iCMS.version.php
@@ -9,5 +9,5 @@
 * @version 6.0.1
 * @$Id: iCMS.version.php 2006 2013-07-22 09:18:24Z coolmoo $
 */
-define('iCMS_VER', 'V6.0.3');//当前版本
-define('iCMS_RELEASE', '20150429');
+define('iCMS_VER', 'V6.0.4');//当前版本
+define('iCMS_RELEASE', '20150511');
