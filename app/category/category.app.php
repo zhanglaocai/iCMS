a00f012f2e01e9f6ff6ed18fec60cf7d93d4b9aa
cool.tea <idreamsoft@qq.com>
2015-06-30 23:06:21 +0800

    增加$category.navArray category.app.php 优化

--- app/category/category.app.php
+++ app/category/category.app.php
@@ -36,8 +36,8 @@ class categoryApp{
         $iurl = iURL::get('category',$category);
 
         if($tpl){
-            if(iPHP::$iTPL_MODE=="html" &&
-                (strstr($category['contentRule'],'{PHP}')
+            if(iPHP::$iTPL_MODE=="html" && (
+                strstr($category['contentRule'],'{PHP}')
                 ||$category['outurl']
                 ||empty($category['mode']))
             ) {return false;}
@@ -46,35 +46,25 @@ class categoryApp{
             $category['mode']=='1' && iCMS::gotohtml($iurl->path,$iurl->href);
         }
         $category['iurl']   = (array)$iurl;
-        $category['url']    = $iurl->href;
-        $category['link']   = "<a href='{$category['url']}' target='_blank'>{$category['name']}</a>";
-        $category['nav']    = $this->get_nav($category);
         $category['subid']  = iCache::get('iCMS/category/rootid',$id);
         $category['subids'] = implode(',',(array)$category['subid']);
-        $category['parent'] = array();
+        $category  = array_merge($category,$this->get_lite($category));
 
-        if($category['rootid']){
-            $_parent            = iCache::get('iCMS/category/'.$category['rootid']);
-            $category['parent'] = $this->get_lite($_parent);
-            unset($_parent);
-        }
-        if($category['password']){
-            $category_auth        = iPHP::get_cookie('category_auth_'.$id);
-            list($ca_cid,$ca_psw) = explode('#=iCMS!=#',authcode($category_auth,'DECODE'));
-        	if($ca_psw!=md5($category['password'])){
-        		iPHP::assign('forward',__REF__);
-	        	iPHP::view('{iTPL}/category.password.htm','category.password');
-	        	exit;
-        	}
-        }
+
+        // if($category['password']){
+        //     $category_auth        = iPHP::get_cookie('category_auth_'.$id);
+        //     list($ca_cid,$ca_psw) = explode('#=iCMS!=#',authcode($category_auth,'DECODE'));
+        // 	if($ca_psw!=md5($category['password'])){
+        // 		iPHP::assign('forward',__REF__);
+	       //  	iPHP::view('{iTPL}/category.password.htm','category.password');
+	       //  	exit;
+        // 	}
+        // }
         if($category['hasbody']){
            $category['body'] = iCache::get('iCMS/category/'.$category['cid'].'.body');
            $category['body'] && $category['body'] = stripslashes($category['body']);
         }
         $category['appid']  = iCMS_APP_CATEGORY;
-        $category['pic']  = get_pic($category['pic']);
-        $category['mpic'] = get_pic($category['mpic']);
-        $category['spic'] = get_pic($category['spic']);
         $category['param'] = array(
             "appid" => $category['appid'],
             "iid"   => $category['cid'],
@@ -88,44 +78,59 @@ class categoryApp{
             $category['mode'] && iCMS::set_html_url($iurl);
             iCMS::hooks('enable_comment',true);
             iPHP::assign('category',$category);
-            if(strstr($tpl,'.htm')){
+            if(strpos($tpl, '.htm')!==false){
             	return iPHP::view($tpl,'category');
             }
             $GLOBALS['page']>1 && $tpl='list';
-            $html	= iPHP::view($category[$tpl.'TPL'],'category.'.$tpl);
+            $html = iPHP::view($category[$tpl.'TPL'],'category.'.$tpl);
             if(iPHP::$iTPL_MODE=="html") return array($html,$category);
         }else{
         	return $category;
         }
     }
-    public function get_nav($C) {
+    public function get_nav($C,&$navArray = array()) {
         if($C) {
             $iurl = (array)$C['iurl'];
-            $link = "<li><a href='{$iurl['href']}'>{$C['name']}</a><span class=\"divider\">".iPHP::lang('iCMS:navTag')."</span></li>";
+            $navArray[]=array(
+                'name'=>$C['name'],
+                'href'=>$iurl['href'],
+            );
             if($C['rootid']){
                 $rc = iCache::get('iCMS/category/'.$C['rootid']);
                 $rc['iurl'] = (array)iURL::get('category',$rc);
-                $nav.= $this->get_nav($rc);
+                $this->get_nav($rc,$navArray);
             }
-            $nav.= $link;
         }
-        return $nav;
+        return $navArray;
     }
     public function get_lite($C){
-        $category                = array();
         $C['iurl'] OR $C['iurl'] = (array)iURL::get('category',$C);
+
+        $category                = array();
         $category['name']        = $C['name'];
         $category['description'] = $C['description'];
         $category['subname']     = $C['subname'];
         $category['sname']       = $C['subname'];
         $category['pic']         = $C['pic'];
-        $category['nav']         = $this->get_nav($C);;
+        $category['navArray']    = $this->get_nav($C);
         $category['url']         = $C['iurl']['href'];
-        $category['link']        = "<a href='{$category['url']}'>{$C['name']}</a>";
+        $category['link']        = "<a href='{$C['url']}'>{$C['name']}</a>";
+        $category['pic']         = get_pic($C['pic']);
+        $category['mpic']        = get_pic($C['mpic']);
+        $category['spic']        = get_pic($C['spic']);
+
         if($C['rootid']){
             $_parent            = iCache::get('iCMS/category/'.$C['rootid']);
             $category['parent'] = $this->get_lite($_parent);
             unset($_parent);
+        }
+
+        $category['nav'] = '';
+        krsort($category['navArray']);
+        if($category['navArray']){
+            foreach ($category['navArray'] as $key => $value) {
+                $category['nav'].="<li><a href='{$value['href']}'>{$value['name']}</a><span class=\"divider\">".iPHP::lang('iCMS:navTag')."</span></li>";
+            }
         }
         return $category;
     }
