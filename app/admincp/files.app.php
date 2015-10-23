bcc3401ade56f6634f12cce654f27c1fb6d69670
cool.tea <cool.tea@qq.com>
2015-10-23 13:15:28 +0800

    iFS::folder

--- app/admincp/files.app.php
+++ app/admincp/files.app.php
@@ -194,7 +194,7 @@ class filesApp{
     }
     function explorer($dir=NULL,$type=NULL){
         iACP::MP('FILE.BROWSE','page');
-        $res    = iPHP::folder($dir,$type);
+        $res    = iFS::folder($dir,$type);
         $dirRs  = $res['DirArray'];
         $fileRs = $res['FileArray'];
         $pwd    = $res['pwd'];
