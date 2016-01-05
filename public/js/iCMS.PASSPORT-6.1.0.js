fffbac61d8ea679260ff3c6fc089bd40cf9eb2bd
cool.tea <idreamsoft@qq.com>
2016-01-05 23:16:53 +0800

    ui

--- public/js/iCMS.PASSPORT-6.1.0.js
+++ public/js/iCMS.PASSPORT-6.1.0.js
@@ -11,46 +11,32 @@ define(["jquery","icms"],function($,iCMS){
     return {
         LOGIN:function (param,success,fail) {
             var param = $.extend(param,{'action': 'login'});
-            if(param.uname==""){
-                iCMS.callback(fail,['uname','请输入用户名']);
-                return false
-            }
-            if(param.pass==""){
-                iCMS.callback(fail,['pass','请输入密码']);
-                return false
-            }
-            if(param.login_seccode){
-                if(param.seccode==""){
-                    iCMS.callback(fail,['seccode','请输入验证码']);
-                    return false
-                }
-            }
-
             $.post(iCMS.api('user'), param, function(ret) {
                 if (ret.code) {
-                    iCMS.callback(success,[ret.forward,ret.msg]);
+                    iCMS.callback(success,ret);
                 } else {
-                    iCMS.callback(fail,[ret.forward,ret.msg]);
+                    iCMS.callback(fail,ret);
                 }
             }, 'json');
         },
         REGISTER:function (param,success,fail) {
             var param = $.extend(param,{'action': 'register'});
+            $.post(iCMS.api('user'), param, function(ret) {
+                if (ret.code) {
+                    iCMS.callback(success,ret);
+                } else {
+                    iCMS.callback(fail,ret);
+                }
+            }, 'json');
         },
         ajax_check:function (param,success,fail) {
-            $.get(iCMS.api('user',"&do=check"),param,
-                // {name: a.name,value: a.value},
-                function(ret) {
-                    if (ret.code) {
-                        $(a).data("check", true);
-                        iCMS.callback(success,[a]);
-                    } else {
-                        $(a).data("check", false);
-                        iCMS.callback(fail,[a,ret.msg]);
-                    }
+            $.get(iCMS.api('user',"&do=check"),param,function(ret) {
+                if (ret.code) {
+                    iCMS.callback(success,ret);
+                } else {
+                    iCMS.callback(fail,ret);
                 }
-            , 'json');
-
+            }, 'json');
         }
     }
 });
