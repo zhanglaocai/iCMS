62539bfda8eb039d597670fe9b2632fd6d1011e7
cool.tea <idreamsoft@qq.com>
2016-04-16 15:13:47 +0800

    VERSION V6.0.13 RELEASE 20160416

--- core/iCMS.version.php
+++ core/iCMS.version.php
@@ -8,5 +8,5 @@
 * @licence http://www.idreamsoft.com/license.php
 * @version 6.0
 */
-define('iCMS_VER', 'V6.0.12');
-define('iCMS_RELEASE', '20160305');
+define('iCMS_VER', 'V6.0.13');
+define('iCMS_RELEASE', '20160416');
