4534c64094629b1e897a3f003b0397128e9525c5
cool.tea <idreamsoft@qq.com>
2017-01-08 14:05:59 +0800

    u

---
+++ app/apps/admincp/apps.iFormer.build.php
@@ -0,0 +1,173 @@
+<div class="fields-fluid">
+  <ul id="custom_field_list">
+  </ul>
+  <div class="clearfloat mt10"></div>
+</div>
+<div class="fields-container">
+  <div class="widget-title">
+    <span class="icon"> <i class="fa fa-cog"></i> </span>
+    <h5 class="brs">字段</h5>
+    <ul class="nav nav-tabs" id="fields-tab">
+      <li class="active"><a href="#fields-base" data-toggle="tab"><i class="fa fa-info-circle"></i> 基础字段</a></li>
+      <li><a href="#fields-func" data-toggle="tab"><i class="fa fa-cog"></i> 功能字段</a></li>
+      <li><a href="#fields-addons" data-toggle="tab"><i class="fa fa-cog"></i> 附加字段</a></li>
+    </ul>
+  </div>
+  <div id="fields-tab-content" class="tab-content">
+    <div id="fields-base" class="tab-pane active">
+      <ul>
+        <li i="layout" tag="br" class="br">
+          <span class="fa fa-arrows-h"></span>
+          <p>换行符</p>
+        </li>
+        <li i="field" tag="input" field="VARCHAR" len="255">
+          <span class="fb-icon fb-icon-input"></span>
+          <p>单行</p>
+        </li>
+        <li i="field" tag="input" field="VARCHAR" len="5120">
+          <span class="fb-icon fb-icon-input"></span>
+          <p>单行长文本</p>
+        </li>
+        <li i="field" tag="textarea" field="TEXT">
+          <span class="fb-icon fb-icon-textarea"></span>
+          <p>多行</p>
+        </li>
+        <li i="field" tag="input" field="VARCHAR" len="255" label="邮箱">
+          <span class="fb-icon fb-icon-mail"></span>
+          <p>邮箱</p>
+        </li>
+        <li i="field" tag="input" type="date" field="INT" len="10" label="日期">
+          <span class="fb-icon fb-icon-date"></span>
+          <p>日期</p>
+        </li>
+        <li i="field" tag="input" type="datetime" field="INT" len="10" label="时间">
+          <span class="timeIcon fb-icon fb-icon-datetime"></span>
+          <p>日期时间</p>
+        </li>
+        <li i="field" tag="input" type="radio" field="TINYINT" len="1" label="单选">
+          <span class="fb-icon fb-icon-radio"></span>
+          <p>单选框</p>
+        </li>
+        <li i="field" tag="input" type="checkbox" field="TINYINT" len="1" label="复选">
+          <span class="fb-icon fb-icon-checkbox"></span>
+          <p>复选框</p>
+        </li>
+        <li i="field" tag="select" type="select" field="TINYINT" len="1" label="列表">
+          <span class="fb-icon fb-icon-dropdown"></span>
+          <p>下拉列表</p>
+        </li>
+        <li i="field" tag="select" type="multiple" field="VARCHAR" len="255" label="多选">
+          <span class="multiselect fb-icon fb-icon-multiselect"></span>
+          <p>多选列表</p>
+        </li>
+        <li i="field" tag="input" type="number" field="TINYINT" len="1" label="数字">
+          <span class="fb-icon fb-icon-number"></span>
+          <p>数字</p>
+        </li>
+        <li i="field" tag="input" type="number" field="INT" len="10" label="数字">
+          <span class="fb-icon fb-icon-number"></span>
+          <p>大数字</p>
+        </li>
+        <li i="field" tag="input" type="number" field="BIGINT" len="20" label="数字">
+          <span class="fb-icon fb-icon-number"></span>
+          <p>超大数字</p>
+        </li>
+        <li i="field" tag="input" type="decimal" field="DECIMAL" len="6,2" label="小数">
+          <span class="fb-icon fb-icon-decimal"></span>
+          <p>小数</p>
+        </li>
+        <li i="field" tag="input" type="percentage" field="DECIMAL" len="3,2" label="百分比" label-after="%">
+          <span class="fb-icon fb-icon-percentage"></span>
+          <p>百分比</p>
+        </li>
+        <li i="field" tag="input" type="currency2" field="INT" len="10" label="货币" label-after="¥">
+          <span class="fb-icon fb-icon-currency2"></span>
+          <p>货币</p>
+        </li>
+        <li i="field" tag="input" field="VARCHAR" len="255" label="链接">
+          <span class="fb-icon fb-icon-url"></span>
+          <p>Url</p>
+        </li>
+        <!--                         <li i="field" fieldtype="32">
+          <span class="lookupIcon fb-icon fb-icon-lookup"></span>
+          <p class="lookupConent">查找</p>
+        </li>
+        <li i="field" fieldtype="14">
+          <span class="addnotesIcon fb-icon fb-icon-addnotes"></span>
+          <p class="addnotestext">添加备注</p>
+        </li>
+        <li i="field" fieldtype="99">
+          <span class="subformIcon fb-icon fb-icon-subform"></span>
+          <p class="subformText">子表单</p>
+        </li>
+        <li i="field" fieldtype="31">
+          <span class="autonumberIcon fb-icon fb-icon-autonumber"></span>
+          <p class="lookupConent">自动编号</p>
+        </li>
+        <li i="field" fieldtype="15">
+          <span class="formulaIcon fb-icon fb-icon-formula"></span>
+          <p class="formulaText">公式</p>
+        </li>
+        <li i="field" fieldtype="36">
+          <span class="signatureIcon fb-icon fb-icon-signature"></span>
+          <p class="lookupConent">签名 </p>
+        </li>
+        <li i="field" fieldtype="30">
+          <span class="usersIcon fb-icon fb-icon-name" style="margin-top:1px;"></span>
+          <p class="lookupConent">用户</p>
+        </li> -->
+      </ul>
+    </div>
+    <div id="fields-func" class="tab-pane">
+      <ul>
+        <li i="field" tag="dialog" field="VARCHAR" len="255" label="选择框">
+          <span class="fb-icon fb-icon-deciton"></span>
+          <p>选择框</p>
+        </li>
+        <li i="field" tag="image" field="VARCHAR" len="255" label="图片">
+          <span class="fb-icon fb-icon-image"></span>
+          <p>图片上传</p>
+        </li>
+        <li i="field" tag="multimage" field="TEXT" label="多图">
+          <span class="fb-icon fb-icon-image"></span>
+          <p>多图上传</p>
+        </li>
+        <li i="field" tag="file" field="VARCHAR" len="255" label="上传">
+          <span class="fb-icon fb-icon-fileupload"></span>
+          <p>上传文件</p>
+        </li>
+        <li i="field" tag="multifile" field="TEXT" label="批量上传">
+          <span class="fb-icon fb-icon-fileupload"></span>
+          <p>批量上传</p>
+        </li>
+        <li i="field" tag="prop" field="VARCHAR" len="255" label="属性">
+          <span class="fb-icon fb-icon-prop"></span>
+          <p>属性</p>
+        </li>
+        <li i="field" tag="seccode" label="验证码">
+          <span class="fb-icon fb-icon-url"></span>
+          <p>验证码</p>
+        </li>
+      </ul>
+    </div>
+    <div id="fields-addons" class="tab-pane">
+      <ul>
+        <li i="field" tag="textarea" field="MEDIUMTEXT" label="超大文本">
+          <span class="fb-icon fb-icon-textarea"></span>
+          <p>超大文本</p>
+        </li>
+        <li i="field" tag="editor" field="MEDIUMTEXT" label="编辑器">
+          <span class="fb-icon fb-icon-richtext"></span>
+          <p>编辑器</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="clearfloat mt10"></div>
+<div class="alert alert-info alert-block">
+  <h5><i class="fa fa-support"></i> 注意事项</h5>
+  <p><i class="fa fa-arrows-h"></i> 换行符 双击可删除</p>
+  <p><i class="fa fa-arrows-h"></i> 换行符 属于占位符 最终界面上将以10px空白替换.效果请参考文章添加页</p>
+  <p>本界面元素只作编辑用,最终界面展现效果请使用预览功能</p>
+</div>
---
+++ app/apps/admincp/apps.iFormer.edit.php
@@ -0,0 +1,164 @@
+<div id="field_edit" class="hide" style="width:600px;">
+  <form id="field_form">
+    <input type="hidden" name="id" id="iFormer-id"/>
+    <input type="hidden" name="tag" id="iFormer-tag"/>
+    <input type="hidden" name="field" id="iFormer-field"/>
+    <div class="input-prepend">
+      <span class="add-on">字段名称</span>
+      <input type="text" name="label" class="span3" id="iFormer-label" value=""/>
+    </div>
+    <span class="help-inline">* 必填</span>
+    <div class="input-prepend">
+      <span class="add-on">字&nbsp;&nbsp;段&nbsp;名</span>
+      <input type="text" name="name" class="span3" id="iFormer-name" value=""/>
+    </div>
+    <span class="help-inline">* 必填</span>
+    <div class="clearfix"></div>
+    <div id="iFormer-value-text">
+      <div class="input-prepend">
+        <span class="add-on">默&nbsp;&nbsp;认&nbsp;值</span>
+        <input type="text" name="value" class="span3" id="iFormer-value" value=""/>
+      </div>
+      <span class="help-inline">选填</span>
+    </div>
+    <div id="iFormer-value-select" class="hide">
+      <div class="input-prepend">
+        <span class="add-on">选项列表</span>
+        <textarea type="text" name="value" class="span3" disabled/></textarea>
+      </div>
+      <span class="help-inline">* 必填.<br />
+        格式: 选项=值;<br />
+      例:电脑=pc</span>
+    </div>
+    <div class="clearfix"></div>
+    <div class="input-prepend">
+      <span class="add-on">字段说明</span>
+      <input type="text" name="help" class="span3" id="iFormer-help" value=""/>
+    </div>
+    <span class="help-inline">选填</span>
+    <div class="input-prepend">
+      <span class="add-on">字段样式</span>
+      <input type="text" name="class" class="span3" id="iFormer-class" value=""/>
+    </div>
+    <span class="help-inline">选填</span>
+    <div class="input-prepend">
+      <span class="add-on">数据长度</span>
+      <input type="text" name="len" class="span3" id="iFormer-len" value=""/>
+    </div>
+    <span class="help-inline">选填</span>
+    <div class="clearfix"></div>
+    <div class="field-tab-box">
+      <ul class="nav nav-tabs" id="field-tab">
+        <li class="active"><a href="#field-tab-1" data-toggle="tab"><i class="fa fa-check-square-o"></i> 验证</a></li>
+        <li><a href="#field-tab-2" data-toggle="tab"><i class="fa fa-cog"></i> 数据处理</a></li>
+        <li><a href="#field-tab-3" data-toggle="tab"><i class="fa fa-info-circle"></i> 提示</a></li>
+        <li><a href="#field-tab-4" data-toggle="tab"><i class="fa fa-user"></i> 用户</a></li>
+        <li><a href="#field-tab-5" data-toggle="tab"><i class="fa fa-cog"></i> 优化</a></li>
+      </ul>
+      <div class="tab-content">
+        <div id="field-tab-1" class="tab-pane active">
+          <div class="input-prepend">
+            <span class="add-on">数据验证</span>
+            <select name="validate[]" id="iFormer-validate" class="chosen-select" style="width:360px;" data-placeholder="请选择数据验证方式..." multiple="multiple">
+              <option value='empty'>不能为空</option>
+              <option value='num'>只能为数字</option>
+              <option value='minmax'>验证范围</option>
+              <option value='len'>字数检测</option>
+              <option value='repeat'>检查重复</option>
+              <option value='email'>E-Mail地址</option>
+              <option value='url'>网址</option>
+              <option value='phone'>手机号码</option>
+              <option value='telphone'>联系电话</option>
+            </select>
+          </div>
+          <span class="help-inline">选填</span>
+          <div class="clearfix"></div>
+          <div id="minmax" class="input-prepend input-append hide">
+            <span class="add-on">验证范围</span>
+            <span class="add-on">最小值</span>
+            <input type="text" name="validate_min" class="span1" id="iFormer-validate_min" value=""/>
+            <span class="add-on">-</span>
+            <input type="text" name="validate_max" class="span1" id="iFormer-validate_max" value=""/>
+            <span class="add-on">最大值</span>
+          </div>
+        </div>
+        <div id="field-tab-2" class="tab-pane">
+          <div class="input-prepend">
+            <span class="add-on">数据处理</span>
+            <select name="fun[]" id="iFormer-fun" class="chosen-select" style="width:360px;" data-placeholder="请选择数据处理方式..." multiple="multiple">
+              <option value='html'>清除HTML</option>
+              <option value='format'>格式化HTML</option>
+              <option value='pinyin'>拼音</option>
+              <option value='explode-n'>分割成数组(换行符)</option>
+              <option value='explode-c'>分割成数组(,)</option>
+              <option value='strtolower'>小写字母</option>
+              <option value='strtoupper'>大写字母</option>
+              <option value='rand'>随机数</option>
+              <option value='json_encode'>数组转json</option>
+              <option value='base64'>base64</option>
+              <option value='serialize'>数组序列化</option>
+              <option value='redirect'>网址跳转</option>
+            </select>
+          </div>
+          <span class="help-inline">选填</span>
+          <div class="input-prepend">
+            <span class="add-on">关联应用</span>
+            <input type="text" name="app" class="span3" id="iFormer-app" value=""/>
+          </div>
+        </div>
+        <div id="field-tab-3" class="tab-pane">
+          <span class="help-inline">支持bootstrap v2.3.2样式 或者请先定义css在填写样式名</span>
+          <div class="clearfix"></div>
+          <div class="input-prepend">
+            <span class="add-on">默认提示</span>
+            <input type="text" name="holder" class="span3" id="iFormer-holder" value=""/>
+          </div>
+          <div class="clearfloat"></div>
+          <div class="input-prepend">
+            <span class="add-on">错误提示</span>
+            <input type="text" name="error" class="span3" id="iFormer-error" value=""/>
+          </div>
+        </div>
+        <div id="field-tab-4" class="tab-pane">
+          <div class="input-prepend">
+            <span class="add-on">用户选项</span>
+            <select name="user[]" id="iFormer-user" class="chosen-select" style="width:360px;" data-placeholder="请选择数据处理方式..." multiple="multiple">
+              <option value='show'>用户后台显示</option>
+              <option value='fill'>用户后台可填写</option>
+            </select>
+          </div>
+          <span class="help-inline">选填</span>
+        </div>
+        <div id="field-tab-5" class="tab-pane">
+          <div class="input-prepend">
+            <span class="add-on">数据优化</span>
+            <select name="db[]" id="iFormer-db" class="chosen-select" style="width:360px;" data-placeholder="请选择数据处理方式..." multiple="multiple">
+              <option value='index'>搜索项/索引</option>
+            </select>
+          </div>
+          <span class="help-inline">选填</span>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
+<script type="text/javascript">
+$(function(){
+  //{handle: ".widget-title"}
+  $( ".fields-container" ).draggable();
+  $("#iCMS-apps").submit(function(){
+    var name =$("#app_name").val();
+    if(name==''){
+      $("#app_name").focus();
+      iCMS.alert("应用名称不能为空");
+      return false;
+    }
+    var app =$("#app_app").val();
+    if(app==''){
+      $("#app_app").focus();
+      iCMS.alert("应用标识不能为空");
+      return false;
+    }
+  })
+})
+</script>
