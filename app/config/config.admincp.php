041f64e346c2f26728c6ab8609d9719fcc2701a0
cool.tea <idreamsoft@qq.com>
2017-03-26 11:20:16 +0800

    [F]修复BUG

--- app/config/config.admincp.php
+++ app/config/config.admincp.php
@@ -35,8 +35,10 @@ class configAdmincp{
         $config['router']['dir']    = rtrim($config['router']['dir'],'/').'/';
         $config['FS']['url']        = trim($config['FS']['url'],'/').'/';
         $config['router']['config'] = json_decode(stripcslashes($_POST['config']['router']['config']),true);
-        $error = json_last_error_msg();
-        $error && iUI::alert("REWRITE配置出错 > JSON -{$error}");
+        if(json_last_error()){
+            $error = json_last_error_msg();
+            $error && iUI::alert("REWRITE配置出错 > JSON -{$error}");
+        }
 
     	foreach($config AS $n=>$v){
     		$this->set($v,$n,0);
