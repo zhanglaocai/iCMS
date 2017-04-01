96944389dd14af188fb8ba021f20b649efa17815
cool.tea <idreamsoft@qq.com>
2017-04-01 14:12:16 +0800

    [A]添加模板标签调节器 call 参数可变    用法    <!--{'类名'|call:'方法':'参数1':'参数2':'参数3':'参数4'}-->    或    <!--{'静态类名::方法'|call:'参数1':'参数2':'参数3':'参数4'}-->

---
+++ iPHP/core/template/plugins/modifier.call.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * iPHP explode modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     call<br>
+ * Date:     2017-04-01
+ * Purpose:  call object method
+ * Input:    object
+ * Example:  {$object|call:'method':'1':'2':'3'}
+ * @author   coolmoo
+ * @version 1.0
+ * @param string
+ * @param string
+ * @return string
+ */
+function tpl_modifier_call($object,$method){
+    $arg_list = func_get_args ();
+    if(strpos($object,'::')===false){
+        $array    = array_slice($arg_list, 2);
+        $call     = array($object,$method);
+    }else{
+        $array    = array_slice($arg_list, 1);
+        $call     = $object;
+    }
+
+	if(!is_callable($call)) return;
+    return call_user_func_array($call,$array);
+}
+
