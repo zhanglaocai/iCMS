8204c6c048e407a02f348fb347117dfb78d2eb03
zhainan13 <876052849@qq.com>
2017-04-12 12:54:51 +0800

    upyun的sdk文件

---
+++ iPHP/library/upyun3/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'user_name' => 'dev',
+    'pwd' => 'xiejing.com',
+    'bucket' => 'dev-xiejing-test',
+	'file_dir'=>'uploads/ueditor/',
+    'picture_path'=>'',//可以存放水印
+);
