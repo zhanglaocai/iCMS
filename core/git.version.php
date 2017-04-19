bed8a2b7746056d2cff55a42db19afefadfb2e54
cool.tea <idreamsoft@qq.com>
2017-04-19 19:48:52 +0800

    [A]添加开发版信息文件

---
+++ core/git.version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* iCMS - i Content Management System
+* Copyright (c) 2007-2012 idreamsoft.com iiimon Inc. All rights reserved.
+*
+* @author coolmoo <idreamsoft@qq.com>
+* @site http://www.idreamsoft.com
+* @licence http://www.idreamsoft.com/license.php
+* @version 6.2.0
+*/
+define('GIT_COMMIT', '1dbc2ca5c1931c7a3229be2aa0c6f57a63ff31e7');
+define('GIT_AUTHOR', 'cool.tea');
+define('GIT_EMAIL', 'idreamsoft@qq.com');
+define('GIT_TIME', '1492602019');
