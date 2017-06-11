aea4a63ce919168c20fb506914570f034c9af407
icmsdev <master@icmsdev.com>
2017-06-11 19:04:41 +0800

    [F]修复用户首页分类管理链接出错

--- core/git.version.php
+++ core/git.version.php
@@ -7,7 +7,7 @@
 * @site https://www.icmsdev.com
 * @licence https://www.icmsdev.com/LICENSE.html
 */
-define('GIT_COMMIT', '43492bc1e704f888177c5808e282de8115c28872');
+define('GIT_COMMIT', '5f1a5bd8ad3786086f5c52273b19812c8dc45706');
 define('GIT_AUTHOR', 'icmsdev');
 define('GIT_EMAIL', 'master@icmsdev.com');
-define('GIT_TIME', '1497087264');
+define('GIT_TIME', '1497178720');
