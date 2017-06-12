2e29c62db7155f0421b501649b0af33e64e275e1
icmsdev <master@icmsdev.com>
2017-06-12 11:43:04 +0800

    [F]修复设置适配跳转后无效果的问题

--- core/git.version.php
+++ core/git.version.php
@@ -7,7 +7,7 @@
 * @site https://www.icmsdev.com
 * @licence https://www.icmsdev.com/LICENSE.html
 */
-define('GIT_COMMIT', 'aea4a63ce919168c20fb506914570f034c9af407');
+define('GIT_COMMIT', '1b1b94546a86f2dfb897be0946d8b344bbfa5b86');
 define('GIT_AUTHOR', 'icmsdev');
 define('GIT_EMAIL', 'master@icmsdev.com');
-define('GIT_TIME', '1497179081');
+define('GIT_TIME', '1497237975');
