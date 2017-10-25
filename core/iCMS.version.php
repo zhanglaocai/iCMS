5c70c1b1f0db0efcf749894948097e2e7abcb4ce
icmsdev <master@icmsdev.com>
2017-10-25 15:19:52 +0800

    VERSION V7.0.4 RELEASE 20171025

--- core/iCMS.version.php
+++ core/iCMS.version.php
@@ -7,5 +7,5 @@
 * @site https://www.icmsdev.com
 * @licence https://www.icmsdev.com/LICENSE.html
 */
-define('iCMS_VERSION', 'V7.0.3');
-define('iCMS_RELEASE', '20170825');
+define('iCMS_VERSION', 'V7.0.4');
+define('iCMS_RELEASE', '20171025');
